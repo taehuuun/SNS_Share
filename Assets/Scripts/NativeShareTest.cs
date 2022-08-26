899c47b859d954c7b32944e9e30f34bb325ebbcf
Hades000 <lsw463@naver.com>
2022-08-27 01:34:43 +0900

    Feat: 이미지를 직접 캐싱 하도록 수정 및 테스트

--- Assets/Scripts/NativeShareTest.cs
+++ Assets/Scripts/NativeShareTest.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class NativeShareTest : MonoBehaviour
 {
+    public Texture2D shareImg;
     public void ClickShare()
     {
-        new NativeShare().AddFile("Assets/Test.png")
+        new NativeShare().AddFile(shareImg)
 		.SetSubject( "TES TEST" ).SetText( "Hello world!" ).SetUrl( "https://www.naver.com/" )
 		.SetCallback( ( result, shareTarget ) => Debug.Log( "Share result: " + result + ", selected app: " + shareTarget ) )
 		.Share();
