7cc732dc96e2922a0fe51bafef755c567ef8d7ee
Hades000 <lsw463@naver.com>
2022-08-30 03:11:24 +0900

    FIx: 캡쳐 프리뷰 화면이 하얗게 나오는 문제 수정

--- Assets/Scripts/ScreenShot.cs
+++ Assets/Scripts/ScreenShot.cs
@@ -12,7 +12,7 @@ public class ScreenShot : MonoBehaviour
     [SerializeField] private Image captureScreen;
     [SerializeField] private bool isCoroutinePlay;
     [SerializeField] private string albumName = "";
-    [SerializeField] private Sprite tmpSprite = null;
+    [SerializeField] private Texture2D tmpTexture = null;
 
     #if UNITY_ANDROID
     private static AndroidJavaClass ajc = new AndroidJavaClass("com.yasirkula.unity.NativeGallery");
@@ -53,8 +53,8 @@ public class ScreenShot : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(0.3f);
 
-        captureScreen.sprite = tmpSprite;
         captureScreen.gameObject.SetActive(true);
+        captureScreen.sprite = Sprite.Create(tmpTexture, new Rect(0,0,Screen.width,Screen.height), new Vector2(0.5f, 0.5f));
         Debug.Log("캡쳐 이미지 활성화");
 
         isCoroutinePlay = false;
@@ -62,66 +62,19 @@ public class ScreenShot : MonoBehaviour
 
     private void CaptureAndSave()
     {
-        Texture2D capture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        
-        tmpSprite = Sprite.Create(capture, new Rect(0,0,capture.width,capture.height), new Vector2(0.5f, 0.5f));
-
-        capture.ReadPixels(new Rect(0,0,Screen.width,Screen.height),0,0);
-        capture.Apply();
+        tmpTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         
 
-         Debug.Log("" + NativeGallery.SaveImageToGallery(capture, albumName,
+        tmpTexture.ReadPixels(new Rect(0,0,Screen.width,Screen.height),0,0);
+        tmpTexture.Apply();
+
+         Debug.Log("" + NativeGallery.SaveImageToGallery(tmpTexture, albumName,
             "Screenshot_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + "{0}.png"));
- 
-        // To avoid memory leaks.
-        // 복사 완료됐기 때문에 원본 메모리 삭제
-
-
-        Destroy(capture);
     }
 
-    // private void ShowCaptureImg()
-    // {
-    //     string path = GetLastPicurePath();
-    //     Debug.Log($"PATH : {path}");
-    //     if(path == null)
-    //         return;
-        
-    //     Texture2D texture = GetScreenShot(path);
-    //     Sprite sp = Sprite.Create(texture,new Rect(0,0,texture.width,texture.height), new Vector2(0.5f,0.5f));
-    //     captureScreen.SetActive(true);
-    //     captureScreen.GetComponent<Image>().sprite = sp;
-    // }
-
-    // private Texture2D GetScreenShot(string path)
-    // {
-    //     Texture2D texture = null;
-
-    //     byte[] fileByte;
-    //     if (File.Exists(path))
-    //     {
-    //         fileByte = File.ReadAllBytes(path);
-    //         texture = new Texture2D(2, 2, TextureFormat.RGB24, false);
-    //         texture.LoadImage(fileByte);
-    //     }
-
-    //     return texture;
-    // }
-
-    // private string GetLastPicurePath()
-    // {
-    //     string path;
-    //     #if !UNITY_EDITOR && UNITY_ANDROID
-    //     path = ajc.CallStatic<string>("GetMediaPath", "Shine Bright");
-    //     #else
-    //     path = Application.persistentDataPath;
-    //     #endif
-
-    //     string[] files = Directory.GetFiles(path,"*.png");
-
-    //     if(files.Length >0)
-    //         return files[files.Length-1];
-    //     else
-    //         return null;
-    // }
+    public void ClosePreview()
+    {
+        tmpTexture = null;
+        captureScreen.gameObject.SetActive(false);
+    }
 }
